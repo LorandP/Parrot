cb0bee9179c9c07d301593306ccfb62d0619c873
Lori <palfalvi.lorand@gmail.com>
2017-01-30 22:51:23 +0200

    Modified the parrotTrouble function.

--- Application.java
+++ Application.java
@@ -6,16 +6,16 @@ public class Application {
     private String parrotTrouble(boolean talk, int hour)
     {
         Parrot parrot = new Parrot(talk);
-        String Adevarat = String.valueOf(true);
-        String Fals = String.valueOf(false);
+        String True = String.valueOf(true);
+        String False = String.valueOf(false);
 
         if (hour < 0 || hour > 23)
             return "Please enter a valid time interval that matches the following range: 0 - 23";
 
         if (((hour >= 0 && hour <= 6) || (hour > 20 && hour <= 23)) && parrot.getValue() == true)
-            return Adevarat;
+            return True;
 
-        return Fals;
+        return False;
     }
 
 
