3885beff763b6e1a937318a82ee271e54d75fe98
Lori <palfalvi.lorand@gmail.com>
2017-01-30 22:49:24 +0200

    Modified the Application class, converted parrotTrouble function into type String.

--- Application.java
+++ Application.java
@@ -1,24 +1,27 @@
 /**
  * Created by Lori on 1/29/2017.
  */
-public class Application
-{
-    private boolean parrotTrouble (boolean talk, int hour)
+public class Application {
+
+    private String parrotTrouble(boolean talk, int hour)
     {
         Parrot parrot = new Parrot(talk);
+        String Adevarat = String.valueOf(true);
+        String Fals = String.valueOf(false);
 
         if (hour < 0 || hour > 23)
-            throw new NumberFormatException("Please enter a valid time interval that matches the following range: 0 - 23");
+            return "Please enter a valid time interval that matches the following range: 0 - 23";
 
         if (((hour >= 0 && hour <= 6) || (hour > 20 && hour <= 23)) && parrot.getValue() == true)
-            return true;
+            return Adevarat;
 
-        return false;
+        return Fals;
     }
-    public static void main (String []args)
-    {
+
+
+    public static void main(String[] args) {
         Application application = new Application();
 
-        System.out.println(application.parrotTrouble(false, 6));
+        System.out.println(application.parrotTrouble(true, 7));
     }
 }
