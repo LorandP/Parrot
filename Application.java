36433d38ca79f253b4d64cc8d2dc2e96c1193bba
Lori <palfalvi.lorand@gmail.com>
2017-02-03 10:38:10 +0200

    Changed the variable names and added javadoc.

--- Application.java
+++ Application.java
@@ -3,19 +3,26 @@
  */
 public class Application {
 
-    private String parrotTrouble(boolean talk, int hour)
-    {
+    /**
+     * This method verifies if the parrot class returns true in a specific time interval.
+     * Returns true if the parrot class returns true from 0 - 6 or 21 - 23, else returns false.
+     *
+     * @param talk a true or false value that is used by the parrot class.
+     * @param hour the hour interval in which the parrot is active or not (true or false).
+     * @return true if it is active during 0 - 6 or 21 -23 else false.
+     */
+    private String parrotTrouble(boolean talk, int hour) {
         Parrot parrot = new Parrot(talk);
-        String True = String.valueOf(true);
-        String False = String.valueOf(false);
+        String stringValueOfTrue = String.valueOf(true);
+        String stringValueOfFalse = String.valueOf(false);
 
         if (hour < 0 || hour > 23)
             return "Please enter a valid time interval that matches the following range: 0 - 23";
 
         if (((hour >= 0 && hour <= 6) || (hour > 20 && hour <= 23)) && parrot.getValue() == true)
-            return True;
+            return stringValueOfTrue;
 
-        return False;
+        return stringValueOfFalse;
     }
 
 
